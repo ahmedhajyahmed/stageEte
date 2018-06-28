8ee89f8a647d8870a39a2eebed97c6daa673bcbf
ahmedhajyahmed <hajyahmedboh@yahoo.com>
2018-06-28 13:30:58 +0200

    installation du congeBundle

---
+++ src/ILANEO/CongeBundle/ILANEOCongeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ILANEO\CongeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ILANEOCongeBundle extends Bundle
+{
+}
---
+++ src/ILANEO/CongeBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ILANEO\CongeBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    }
+}
