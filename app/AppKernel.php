089d7fd25542948f395cb6cc28247185ebb2feba
ahmedhajyahmed <hajyahmedboh@yahoo.com>
2018-07-23 11:30:21 +0200

    résoluton du conflit

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,8 +19,8 @@ class AppKernel extends Kernel
             #new AppBundle\AppBundle(),
             new ILANEO\CongeBundle\ILANEOCongeBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-            new ADesigns\CalendarBundle\ADesignsCalendarBundle(),
+            //new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+           // new ADesigns\CalendarBundle\ADesignsCalendarBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
