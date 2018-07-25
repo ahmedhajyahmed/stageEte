ee8d7941161a7da56a3b642afa0507408e55db88
ahmedhajyahmed <hajyahmedboh@yahoo.com>
2018-07-25 11:46:02 +0200

    dernier commit

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,9 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             #new AppBundle\AppBundle(),
             new ILANEO\CongeBundle\ILANEOCongeBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-           // new ADesigns\CalendarBundle\ADesignsCalendarBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
