4ca3dea3ce60176cc039976cd76fbf35c2e54296
malak9 <benramdhanmalak@gmail.com>
2018-07-31 09:10:45 +0100

    liste

--- src/ILANEO/CongeBundle/Controller/AdminController.php
+++ src/ILANEO/CongeBundle/Controller/AdminController.php
@@ -8,7 +8,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use ILANEO\CongeBundle\form\RechercheType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 class AdminController extends Controller
 {
 
@@ -55,7 +60,7 @@ class AdminController extends Controller
 
 
 
-
+    //page liste employés
    public function listeEmployesAction (Request  $request)
     {
 
@@ -104,6 +109,99 @@ $entities = $this->getR('knp_paginator')->paginate(
 return $this->redirectToRoute('ilaneo_conge_connexion');
 }*/
 
+/*public function editEmpAction( $id,Request $request  )
+{ 
+    $user=$this->getDoctrine()->getRepository('ILANEOCongeBundle:User')->find($id);
+    
+    $user->setFamilySituation($user->getFamilySituation());
+    $user->setChildren($user->$getChildren());
+    $user->setPhone($user->$getPhone());
+    $user->setEmail($user->$getEmail());
+    $user->setStatus($user->$getStatus());
+    $user->setPost($user->$getPost());
+    $user->setBankAccount($user->$getBankAccount());
 
+    
+    $form = $this->createFormBuilder($user)
+        ->add('familySituation',ChoiceType::class,array('choices'=> array('célibataire'=>'célibataire','marié'=>'marié'),'expanded'=>true))
+        ->add('children',IntegerType::class)
+        ->add('phone', IntegerType::class)
+        ->add('email', EmailType::class)
+        ->add('status',ChoiceType::class, array('choices'=>array('stagiaire'=>'stagiaire','employé'=>'employé','ancien employé'=>'ancien employé')))
+        ->add('post',TextType::class)
+        ->add('bankAccount',IntegerType::class)
+        ->getForm();
+
+    $form ->handleRequest($request);
+
+    if($form->isSubmitted() && $forl->isValid()){
+        $familySituation=$form['familySituation']->getData();
+        $children=$form['children']->getData();
+        $phone=$form['phone']->getData();
+        $email=$form['email']->getData();
+        $status=$form['status']->getData();
+        $post=$form['post']->getData();
+        $bankAccount=$form['bankAccount']->getData();
+
+        $em = $this->getDoctrine()->getManager();
+        $user=$em->getRepository('ILANEOCongeBundle:User')->find($id);
+         
+        $user->setFamilySituation($familySituation);
+        $user->setChildren($children);
+        $user->setPhone($phone);
+        $user->setEmail($email);
+        $user->setStatus($status);
+        $user->setPost($post);
+        $user->setBankAccount($bankAccount);
+
+
+        $em->flush();
+          $this->addFlash('employé modifié');
+        return $this->redirectToRoute('ilaneo_liste_employes');
+    }
+
+    // traitement si méthode get
+    return $this->render('@ILANEOConge/employe/modifierEmp.html.twig',array('user'=>$user,'form'=>$form->createView()));
+}
+
+*/
+public function editEmpAction( $id,Request $request  )
+{
+
+  //  $session = $request->getSession();
+   // $id = $session->get('id');
+    $em = $this->getDoctrine()->getManager();
+    $user = $em->getRepository('ILANEOCongeBundle:User')->find($id);
+ // On crée le FormBuilder grâce au service form factory
+ $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $user);
+
+ // On ajoute les champs de l'entité que l'on veut à notre formulaire
+ $formBuilder
+     ->add('familySituation',ChoiceType::class,array('choices'=> array('célibataire'=>'célibataire','marié'=>'marié'),'expanded'=>true))
+     ->add('children',IntegerType::class)
+     ->add('phone', IntegerType::class)
+     ->add('email', EmailType::class)
+     ->add('status',ChoiceType::class, array('choices'=>array('stagiaire'=>'stagiaire','employé'=>'employé','ancien employé'=>'ancien employé')))
+     ->add('post',TextType::class)
+     ->add('bankAccount',IntegerType::class);
+
+ //on recupére la formulaire
+ $form = $formBuilder->getForm();
+
+ //traitement si le formulaire est envoyé et est valide
+ if($request->isMethod('POST') && $form->handleRequest($request)->isValid()){
+
+    //$em=$this->getDoctrine()->getManager();
+  //  $em->persist($user);
+     $em->flush();
+
+     $request->getSession()->getFlashBag()->add("notice","Vos changements ont été enregistré avec succée.");
+     return $this->redirectToRoute('ilaneo_liste_employes');
+ }
+
+ // traitement si méthode get
+ return $this->render('@ILANEOConge/admin/modifierEmp.html.twig',array('form'=>$form->createView()));
+
+}
 
 }
